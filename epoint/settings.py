d66a9963981519f154820c8fe5eca97bafb413db
Rahul <rahul@Rahuls-MacBook-Air.local>
2025-11-12 12:44:16 +0530

    Done

--- epoint/settings.py
+++ epoint/settings.py
@@ -42,8 +42,9 @@ WSGI_APPLICATION = 'epoint.wsgi.application'
 
 
 DATABASES = {
-    "default": dj_database_url.config(
-        default=os.getenv("DATABASE_URL")
+    'default': dj_database_url.config(
+        default='sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3'),
+        conn_max_age=600,
     )
 }
 
